d834e8340bd0a8c2483fb0caf095b047098f7f28
Fouad <fouad.mansour30788@gmail.com>
2026-08-25 13:33:11 +0300

    Multi-image galleries: HP OmniBook 5, ProBook 4, HP 17 and 250r (batch 4)
    Completes all 17 HP laptops in the catalog. AG1175CL and AF1055CL
    reuse the AG1075CL/AF1017WM photos since they're the identical chassis
    with only RAM/CPU-tier differing, not the industrial design. HP
    17-CN5085CL only got one clean colour-matched addition -- the other
    candidates found were marketing composites with text overlays or the
    wrong (silver) colourway.

---
+++ supabase/migrations/032_multi_image_hp_batch4.sql
@@ -0,0 +1,40 @@
+UPDATE products SET images = ARRAY[
+  '/products/hp-omnibook-5-16-ag1075cl.jpg',
+  '/products/hp-omnibook-5-16-ag1075cl-angle.jpg',
+  '/products/hp-omnibook-5-16-ag1075cl-rear.jpg'
+] WHERE slug = 'hp-omnibook-5-16-ag1075cl';
+
+UPDATE products SET images = ARRAY[
+  '/products/hp-omnibook-5-16-af1017wm.jpg',
+  '/products/hp-omnibook-5-16-af1017wm-front.jpg',
+  '/products/hp-omnibook-5-16-af1017wm-angle.jpg'
+] WHERE slug = 'hp-omnibook-5-16-af1017wm';
+
+UPDATE products SET images = ARRAY[
+  '/products/hp-omnibook-5-16-ag1175cl.jpg',
+  '/products/hp-omnibook-5-16-ag1075cl-angle.jpg',
+  '/products/hp-omnibook-5-16-ag1075cl-rear.jpg'
+] WHERE slug = 'hp-omnibook-5-16-ag1175cl';
+
+UPDATE products SET images = ARRAY[
+  '/products/hp-omnibook-5-16-af1055cl.jpg',
+  '/products/hp-omnibook-5-16-af1017wm-front.jpg',
+  '/products/hp-omnibook-5-16-af1017wm-angle.jpg'
+] WHERE slug = 'hp-omnibook-5-16-af1055cl';
+
+UPDATE products SET images = ARRAY[
+  '/products/hp-probook-4-g1i-16-at7k5av.jpg',
+  '/products/hp-probook-4-g1i-16-at7k5av-open.jpg',
+  '/products/hp-probook-4-g1i-16-at7k5av-front.jpg'
+] WHERE slug = 'hp-probook-4-g1i-16-at7k5av';
+
+UPDATE products SET images = ARRAY[
+  '/products/hp-17-cn5085cl.jpg',
+  '/products/hp-17-cn5085cl-front.jpg'
+] WHERE slug = 'hp-17-cn5085cl';
+
+UPDATE products SET images = ARRAY[
+  '/products/hp-250r-g9-b39zcat.jpg',
+  '/products/hp-250r-g9-b39zcat-angle.jpg',
+  '/products/hp-250r-g9-b39zcat-front.jpg'
+] WHERE slug = 'hp-250r-g9-b39zcat';
