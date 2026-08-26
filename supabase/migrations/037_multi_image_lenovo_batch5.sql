11634f71119d5f9c08cde98872b3e98ea28d3cdb
Fouad <fouad.mansour30788@gmail.com>
2026-08-26 04:07:59 +0300

    Multi-image galleries: Lenovo batch 5 (IP1 15AMN7, IP1 15IRU7 Cloud Grey)
    Completes the IP1 15-series entries. Both sourced with exact-SKU
    matches; skipped a same-SKU candidate with a Latin American retailer's
    spec-overlay marketing template covering most of the laptop.

---
+++ supabase/migrations/037_multi_image_lenovo_batch5.sql
@@ -0,0 +1,9 @@
+UPDATE products SET images = ARRAY[
+  '/products/lenovo-ip1-15amn7-82vg00wxus.jpg',
+  '/products/lenovo-ip1-15amn7-angle.jpg'
+] WHERE slug = 'lenovo-ip1-15amn7-82vg00wxus';
+
+UPDATE products SET images = ARRAY[
+  '/products/lenovo-ip1-15iru7-83b40008us.jpg',
+  '/products/lenovo-ip1-15iru7-83b40008us-front.jpg'
+] WHERE slug = 'lenovo-ip1-15iru7-83b40008us';
