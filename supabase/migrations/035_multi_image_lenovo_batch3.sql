e4b8478c4fe7c7fdad95e8d84a93db51172de4ea
Fouad <fouad.mansour30788@gmail.com>
2026-08-26 03:21:48 +0300

    Multi-image galleries: Lenovo batch 3 (IdeaPad 5 2-in-1 x2, V14 G5 IRL)
    IdeaPad 5 2-in-1 16 and 14IAL10 each get real angle/front shots
    (exact-SKU or same-chassis matches from Amazon/Newegg/Microless since
    Ayoub doesn't stock these). V14 G5 IRL only got one addition -- a
    second candidate photo turned out to be a different grey colourway,
    not our Business Black unit, so it was left out.

---
+++ supabase/migrations/035_multi_image_lenovo_batch3.sql
@@ -0,0 +1,15 @@
+UPDATE products SET images = ARRAY[
+  '/products/lenovo-ideapad-5-2-in-1-16-83ks001bus.jpg',
+  '/products/lenovo-ideapad-5-2-in-1-16-83ks001bus-front.jpg'
+] WHERE slug = 'lenovo-ideapad-5-2-in-1-16-83ks001bus';
+
+UPDATE products SET images = ARRAY[
+  '/products/lenovo-ideapad-5-2-in-1-14ial10-14ial10-83kr0047us.jpg',
+  '/products/lenovo-ideapad-5-2-in-1-14ial10-angle.jpg',
+  '/products/lenovo-ideapad-5-2-in-1-14ial10-front.jpg'
+] WHERE slug = 'lenovo-ideapad-5-2-in-1-14ial10-14ial10-83kr0047us';
+
+UPDATE products SET images = ARRAY[
+  '/products/lenovo-v14-g5-irl-83hda06jus.jpg',
+  '/products/lenovo-v14-g5-irl-angle.jpg'
+] WHERE slug = 'lenovo-v14-g5-irl-83hda06jus';
